73e71ed18ad4a63c98ece516662f6a5dedcc1e0f
Ritwik Bhanot <rbhanot999@gmail.com>
2020-12-25 19:33:19 +0530

    added api routes for login and register

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
     'mongodb+srv://rbhanot999:mqYR7u5W2BhFBqci@cluster0.nl9bg.mongodb.net/project-fullstack-auth?retryWrites=true&w=majority',
+  secretOrKey: 'secret',
 };
